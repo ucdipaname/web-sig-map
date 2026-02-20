8a4bd58a75fc87a1707e9fe128ddd05d68e3824e
ucdipaname <hnicos2@gmail.com>
2026-02-20 15:22:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+// Créer la carte centrée sur Paris (exemple)
+var map = L.map('map').setView([48.8566, 2.3522], 13);
+
+// Ajouter un fond de carte OpenStreetMap
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
 
